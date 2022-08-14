334391069394defd366933f3455f03170d7e1a30
Folf <itsfolf@gmail.com>
2022-08-14 01:23:38 +0100

    Install script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Discord Screenshare Linux
 WIP repo that adds Discord screenshare support for Wayland and Pipewire aswell as audio sharing, without the need to use the web client.
+
+## Install
+```
+sh -c "$(curl https://raw.githubusercontent.com/fuwwy/Discord-Screenshare-Linux/main/scripts/install.sh -sSfL)"
+```
---
+++ scripts/install.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+username=$(logname)
+dirs=(/home/$username/.config/discord*/*/modules/discord_voice)
+len=${#dirs[@]}
+
+if [[ $len -eq 0 ]]; then
+    echo "No Discord installation found"
+    exit 1
+fi
+
+if [[ $len -gt 1 ]]; then
+    echo "Multiple Discord installations found"
+    echo "Please select one:"
+    for i in "${!dirs[@]}"; do
+        echo -n "$((i+1)) "
+        echo "${dirs[$i]}" | cut --delimiter='/' --fields=5
+    done
+    read -p "Enter selection: " selection
+    selectedIndex=$((selection-1))
+    selected=${dirs[$selectedIndex]}
+    if [[ $selectedIndex -lt 0 || $selectedIndex -ge $len ]]; then
+        echo "Invalid selection"
+        exit 1
+    fi
+else
+    selected=${dirs[0]}
+fi
+
+echo "Installing LinuxFix to $selected..."
+
+chmod 644 $selected/index.js
+
+separator="\/\/ ==Discord Screenshare Linux Fix=="
+
+# Remove any existing installation
+sed -i.bak '/^'"$separator"'/,/^'"$separator"'/{/^#/!{/^\$/!d}}' $selected/index.js
+
+#Download latest
+downloadUrl=$(wget -qO- https://api.github.com/repos/fuwwy/Discord-Screenshare-Linux/releases/latest | grep browser_download_url | cut -d '"' -f 4)
+wget -O $selected/linux-fix.node -q --show-progress "$downloadUrl"
+code=$(wget -qO- https://raw.githubusercontent.com/fuwwy/Discord-Screenshare-Linux/main/scripts/patch.js)
+
+#echo -e "\n" >> $selected/index.js
+echo "$code" >> $selected/index.js
+
+chmod 444 $selected/index.js
